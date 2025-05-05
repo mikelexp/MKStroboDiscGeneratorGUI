682aa3da309f1471087f45ac48541c2f28d8f9f0
Miguel Scaramozzino <mikele@gmail.com>
2025-05-05 03:25:48 -0300

    avs y compilacion para windows

---
+++ build-nuitka-win.cmd
@@ -0,0 +1,7 @@
+venv\Scripts\python -m nuitka stroboscope_multi_rings_generator.py ^
+--standalone ^
+--enable-plugin=pyqt6 ^
+--include-module=svgwrite ^
+--include-module=svglib ^
+--include-module=tempfile ^
+--onefile
--- build-nuitka.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source venv/bin/activate
-pip install pyinstaller
-#python -m nuitka --enable-plugin=pyqt6 --standalone --onefile stroboscope_generator.py
-python -m nuitka --standalone --onefile --include-data-dir=/usr/lib64/qt6/plugins=PyQt6/Qt6/plugins stroboscope_generator.py
-echo "The executable has been created in the current folder."
-deactivate
