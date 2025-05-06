c68dcb0c668bdc97b4f59fd9f4e30c488b3a48c7
Miguel Scaramozzino <mikele@gmail.com>
2025-05-06 01:26:22 -0300

    avs

--- build-nuitka-linux.sh
+++ build-nuitka-linux.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-
-#source venv/bin/activate
-#python -m nuitka --enable-plugin=pyqt6 --standalone --onefile stroboscope_generator.py
-#python -m nuitka --standalone --onefile --include-data-dir=/usr/lib64/qt6/plugins=PyQt6/Qt6/plugins stroboscope_generator.py
-
 venv/bin/python -m nuitka stroboscope_multi_rings_generator.py \
 --standalone \
 --enable-plugin=pyqt6 \
 --include-module=svgwrite \
 --include-module=svglib \
+--include-module=tempfile \
 --include-module=reportlab \
 --include-package=reportlab \
---include-module=tempfile \
 --onefile
 
 echo "The executable has been created in the current folder."
--- build-nuitka-win.cmd
+++ build-nuitka-win.cmd
@@ -4,5 +4,7 @@ venv\Scripts\python -m nuitka stroboscope_multi_rings_generator.py ^
 --include-module=svgwrite ^
 --include-module=svglib ^
 --include-module=tempfile ^
+--include-module=reportlab ^
+--include-package=reportlab ^
 --windows-console-mode=disable ^
 --onefile
